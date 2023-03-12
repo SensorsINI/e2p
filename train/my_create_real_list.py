e4686ed93962c42a2bc188e40e04742b3d8e0acc
TaylorMei <845879017@qq.com>
2023-03-12 21:55:24 +0800

    2023.03.12 - Haiyang Mei - add instructions

--- train/my_create_real_list.py
+++ train/my_create_real_list.py
@@ -10,13 +10,13 @@
 """
 import os
 
-root = '/home/mhy/firenet-pdavis/data/real'
-txt_path = '/home/mhy/firenet-pdavis/data/movingcam/train_test_real.txt'
+root = './data/new/real'
+txt_path = './data/new/real.txt'
 
 list = sorted([x for x in os.listdir(root) if x.endswith('.h5')])
 paths = []
 for name in list:
-    path = os.path.join(root, name).replace('/home/mhy/firenet-pdavis', '.')
+    # path = os.path.join(root, name).replace('/home/mhy/firenet-pdavis', '.')
     paths.append(path)
 
 with open(txt_path, 'w') as f:
