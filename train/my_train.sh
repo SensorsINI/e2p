7292578d9208443367727d6ec6780a34a77b4977
Tobi Delbruck <todelbru@ethz.ch>
2023-03-15 17:08:04 +0100

    added /bin/bash and made executable

--- train/my_train.sh
+++ train/my_train.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
 # Train a model from scratch
 export CUDA_VISIBLE_DEVICES=0 # set chosen GPU if there is more than one. Use nvidia-smi -L to list them
+pdavis='/home/tobi/pdavis_demo'
 #python train.py --config ./config_v/v16_b_c16_i.json --limited_memory
 #python train.py --config ./config_v/v16_so.json --limited_memory
-python train.py --config ./e2p.json --limited_memory # limited_memory reduces open file memory use
+#python train.py --config ./e2p.json --limited_memory # limited_memory reduces open file memory use
 
 # Train model from specific checkpoint
 #export CUDA_VISIBLE_DEVICES=0
 #python train.py --config ./config_v/v16_woln_ft_mix.json --limited_memory --resume /home/mhy/firenet-pdavis/ckpt/models/v16_woln/1018_202813/model_best.pth
+
+# fine tune published model with new data; see README.md to add new data
+config="e2p.json"
+echo "training with config $config"
+python train.py -c $config --limited_memory --resume $pdavis/e2p.pth
